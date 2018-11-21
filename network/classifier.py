93dc39823645bfebcfb562dd734528dc5c3ce12f
zms <zhangmaosen@pku.edu.cn>
2018-11-21 19:10:29 +0800

    use weighted loss

--- network/classifier.py
+++ network/classifier.py
@@ -13,8 +13,8 @@ class Classifier(object):
     def softmax_cross_entropy(self, x):
         with tf.name_scope("loss"):
             label_onehot = tf.one_hot(indices=self.label, depth=FLAGS.num_classes, dtype=tf.int32)
-            # loss = tf.losses.softmax_cross_entropy(onehot_labels=label_onehot, logits=x, weights=self.weights)
-            loss = tf.losses.softmax_cross_entropy(onehot_labels=label_onehot, logits=x)
+            loss = tf.losses.softmax_cross_entropy(onehot_labels=label_onehot, logits=x, weights=self.weights)
+            # loss = tf.losses.softmax_cross_entropy(onehot_labels=label_onehot, logits=x)
             tf.summary.scalar('loss', loss)
             return loss
 
